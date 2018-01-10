5f01acb116f352b57cbb4d7c59b7919fb3f73c82
Muhammad Rahmatullah <rahmatthea5@gmail.com>
2018-01-10 10:55:31 +0700

    remove page from params

--- lib/jbuilder/pagination/pages.rb
+++ lib/jbuilder/pagination/pages.rb
@@ -14,21 +14,19 @@ class Jbuilder
 
   def pages_from(collection)
     {}.tap do |pages|
-      pages[:self] = collection.current_page
-      return pages if collection.total_pages == ONE_PAGE
+      # pages[:self] = collection.current_page
+      # return pages if collection.total_pages == ONE_PAGE
 
-      unless collection.current_page == ONE_PAGE
-        pages[:first] = ONE_PAGE
-        pages[:prev]  = collection.current_page - ONE_PAGE
-        pages[:last]  = nil
-      end
+      # unless collection.current_page == ONE_PAGE
+      #   pages[:first] = ONE_PAGE
+      #   pages[:prev]  = collection.current_page - ONE_PAGE
+      # end
 
-      unless collection.current_page == collection.total_pages
-        pages[:next] = collection.current_page + ONE_PAGE
-        pages[:last] = collection.total_pages
-        pages[:prev] = nil
+      # unless collection.current_page == collection.total_pages
+      #   pages[:next] = collection.current_page + ONE_PAGE
+      #   pages[:last] = collection.total_pages
 
-      end
+      # end
     end
   end
 
@@ -37,8 +35,6 @@ class Jbuilder
   end
 
   def is_paginated?(collection)
-    collection.respond_to?(:current_page) &&
-      collection.respond_to?(:total_pages) &&
-      collection.respond_to?(:size)
+    collection.respond_to?(:current_page) && collection.respond_to?(:total_pages) && collection.respond_to?(:size)
   end
 end
